11ecd1731a711a4b709187910b3924e5db730fb2
Michał Brusiło <mibru@interia.pl>
2018-05-23 14:13:46 +0200

    Fix bug in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -71,7 +71,7 @@ var Counter = React.createClass({
 var element = React.createElement('div', {className: 'counters'},
 		React.createElement(Counter),
 		React.createElement(Counter),
-    	React.createElement(Counter),                              
+    	React.createElement(Counter)                             
 	);
 
 ReactDOM.render(element, document.getElementById('app'));
